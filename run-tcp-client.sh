aa8471e07045294fd784ef89def7eef7397ac44c
Jerrico Gamis <jecklgamis@gmail.com>
2020-04-20 08:55:51 +1000

    Fix run scripts

--- run-tcp-client.sh
+++ run-tcp-client.sh
@@ -3,4 +3,4 @@ set -ex
 TUNNEL_NAME=${TUNNEL_NAME:-tun2}
 SERVER_IP=${SERVER_IP:-some-remote-ip}
 SERVER_PORT=${SERVER_PORT:-5050}
-./tcp_client -i "${TUNNEL_NAME}" -s "${SERVER_IP}" -p "${SERVER_PORT}" -d
+./tcp_client -i "${TUNNEL_NAME}" -s "${SERVER_IP}" -p "${SERVER_PORT}" -v
--- run-tcp-server.sh
+++ run-tcp-server.sh
@@ -2,4 +2,4 @@
 set -ex
 TUNNEL_NAME=${TUNNEL_NAME:-tun2}
 SERVER_PORT=${SERVER_PORT:-5050}
-./tcp_server -i "${TUNNEL_NAME}" -p "${SERVER_PORT}" -d
+./tcp_server -i "${TUNNEL_NAME}" -p "${SERVER_PORT}" -v
